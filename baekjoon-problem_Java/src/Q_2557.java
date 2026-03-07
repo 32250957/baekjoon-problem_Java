1b343374bbf8694afa31a476b8c619f9ff7fd5d8
32250957 <kyw060423@dankook.ac.kr>
2026-03-07 22:56:24 +0900

    2557번 작성

---
+++ baekjoon-problem_Java/src/Q_2557.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Q_2557 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        wr.write("Hello World!");
+        wr.flush();
+    }
+}
