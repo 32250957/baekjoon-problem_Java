3fbbb125fb17fe4f5e6308e1d2fbd6fd928decd0
32250957 <kyw060423@dankook.ac.kr>
2026-03-10 12:07:47 +0900

    수정 중

--- baekjoon-problem_Java/src/Q_11725.java
+++ baekjoon-problem_Java/src/Q_11725.java
@@ -1,9 +1,13 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Node1 {
     int data;
-    Node1 
+    List<Node1> list = new ArrayList<>();
+
+    Node1(int data) {
+        this.data = data;
+    }
 }
 
 public class Q_11725 {
@@ -11,12 +15,18 @@ public class Q_11725 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        Map<Integer, Node1> map = new HashMap<>();
+
         int N = Integer.parseInt(br.readLine());
 
         for(int i=0;i<N-1;i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int node1 = Integer.parseInt(st.nextToken());
             int node2 = Integer.parseInt(st.nextToken());
+
+            if(node1 == 1) {
+                
+            }
         }
     }
 }
